bba75c107b7fb07c833b7808d07a4483241983b5
PassionateProgrammers <jameskirk561@gmail.com>
2023-12-03 23:28:13 -0500

    create and delete working

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -30,6 +30,22 @@ const getWorkout = async (req, res) => {
 //create new workout
 const createWorkout = async (req, res) => {
     const {title, load, reps} = req.body
+
+    let emptyFields = []
+
+    if(!title) {
+        emptyFields.push('title')
+    }
+    if(!load) {
+        emptyFields.push('load')
+    }
+    if(!reps) {
+        emptyFields.push('reps')
+    }
+    if(emptyFields.length > 0) {
+        return res.status(400).json({ error: 'All fields required', emptyFields})
+    }
+
     //add to db
     try {
         const workout = await Workout.create({title, load, reps})
