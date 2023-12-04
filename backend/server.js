da0a01818a2785d43940866f2a916db4b0309da1
PassionateProgrammers <jameskirk561@gmail.com>
2023-12-04 09:17:00 -0500

    login validation

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const app = express()
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 app.use(express.json())
 app.use((req, res, next) => {
@@ -14,6 +15,7 @@ app.use((req, res, next) => {
 
 //route
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
